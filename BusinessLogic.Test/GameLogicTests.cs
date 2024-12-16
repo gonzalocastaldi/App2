598032b8941365dcd5133a3b38e9843c8bdbfa5c
gonzalocastaldi <GC292613@fi365.ort.edu.uy>
2024-12-16 00:47:16 -0300

    Added BusinesLogicTets project

---
+++ BusinessLogic.Test/GameLogicTests.cs
@@ -0,0 +1,6 @@
+﻿namespace BusinessLogic.Test;
+
+public class GameLogicTests
+{
+    
+}
---
+++ BusinessLogic.Test/UserLogicTests.cs
@@ -0,0 +1,10 @@
+namespace BusinessLogic.Test;
+
+[TestClass]
+public class UserLogicTests
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
