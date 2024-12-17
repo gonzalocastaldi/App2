babba1d9de8e7a34be94ba92851666dad98d8745
gonzalocastaldi <GC292613@fi365.ort.edu.uy>
2024-12-17 00:53:51 -0300

    tests

--- Domain.Test/GameTests.cs
+++ Domain.Test/GameTests.cs
@@ -1,6 +1,16 @@
-﻿namespace Domain.Test;
+﻿using App2;
 
+namespace Domain.Test;
+
+[TestClass]
 public class GameTests
 {
-    
+    [TestMethod]
+    public void  CreateUser_validParameters_createsUser()
+    {
+        User user = new User();
+        user.UserName = "test";
+        user.Password = "test";
+        Assert.AreEqual("test", user.UserName);
+    }
 }
